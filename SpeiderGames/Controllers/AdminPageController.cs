9e12b74d1a174b657d6f1fb44840b83894ea0c71
hallvman <hallvman@stud.ntnu.no>
2024-01-15 20:46:46 +0100

    Made the layout for the website work

---
+++ SpeiderGames/Controllers/AdminPageController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SpeiderGames.Models;
+using System.Diagnostics;
+
+namespace SpeiderGames.Controllers
+{
+    public class AdminPageController : Controller
+    {
+        private readonly ILogger<AdminPageController> _logger;
+
+        public AdminPageController(ILogger<AdminPageController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View("/Views/Pages/AdminPage.cshtml");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
